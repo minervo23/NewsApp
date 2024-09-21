7f911793895743df40e4fd59e6f738995bfa9af2
Batuhan Kabaktepe <utab.beta@gmail.com>
2024-09-21 11:50:38 +0200

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Batuhan Kabaktepe on 21.09.24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
