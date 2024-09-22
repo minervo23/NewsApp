f610cc70de175362f9d062a03bbe2e8f39081281
Batuhan Kabaktepe <utab.beta@gmail.com>
2024-09-22 15:38:03 +0200

    Revert "WIP"
    This reverts commit de12a0b48476497c3f15260b8398eae5fd7836f9.

--- NewsApp/NewsAppApp.swift
+++ NewsApp/NewsAppApp.swift
@@ -9,12 +9,9 @@ import SwiftUI
 
 @main
 struct NewsAppApp: App {
-    @StateObject var articleBookmarkVM = ArticleBookmarkViewModel.shared
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(articleBookmarkVM)
         }
     }
 }
